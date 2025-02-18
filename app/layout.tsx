dd82cb262d786903cb34f63f06e4450b0fefc9c4
Evan Huang <evan.huang000@proton.me>
2025-02-18 20:48:22 +0800

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type React from "react"
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import { Toaster } from "@/components/ui/toaster"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "EAuth | Secure 2FA code management application",
+  description: "Secure 2FA code management application",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+
+
+import './globals.css'
